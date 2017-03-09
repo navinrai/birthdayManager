49bb500b8f627ffb1e900441bdaca4aeb9047cb4
Aditi Agarwal <umesh.soni@newgen.co.in>
2017-03-09 11:42:20 +0530

    TestCommitByGitBash

---
+++ BirthdayManager/src/main/java/com/birthdaymanager/BirthdayManagerApplication2.java
@@ -0,0 +1,12 @@
+package com.birthdaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayManagerApplication.class, args);
+	}
+}
